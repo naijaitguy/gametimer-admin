b83ac921d1df1e04da11d5da8eab6be168eb860e
babatunde olusegun <thenaijaitguy@gmail.com>
2022-02-10 19:10:37 +0100

    add 4200 origin

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ const AuthHelper = require("./_Helpers/Auth_Helper");
 var app = express();
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://localhost:4200',
+    origin: 'http://localhost:3000',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
